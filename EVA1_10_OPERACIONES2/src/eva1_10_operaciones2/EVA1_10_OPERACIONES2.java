c1b5eb5fa714aae537b368fefd6a8e3ee0b5da26
luisjgro <L24550278@chihuahua2.tecnm.mx>
2024-09-03 17:48:30 -0600

    Add files via upload

---
+++ EVA1_10_OPERACIONES2/src/eva1_10_operaciones2/EVA1_10_OPERACIONES2.java
@@ -0,0 +1,16 @@
+package eva1_10_operaciones2;
+
+public class EVA1_10_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        String nombre = "Luis Javier";
+        String apellido = "Guerrero Arellano";
+        String nomCom;
+        
+        nomCom = nombre + " " + apellido;
+        System.out.println("Nombre del usuario: " + nombre);
+        System.out.println("Apellido del usuario: " + apellido);
+        System.out.println("Nombre completo del usuario: " + nomCom);
+    }
+    
+}
